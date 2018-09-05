15388964d0ff5cd9dd95abdbede6e1dd1e5ef161
Alexander Skou <alexander.skou@gmail.com>
2018-09-05 15:08:06 +0200

    Jeg har tilføjet ViTesterSkuEndnu

--- Test1/src/martnersoed/martn.java
+++ Test1/src/martnersoed/martn.java
@@ -1,5 +1,7 @@
 package martnersoed;
 
+// DETTTE ER ALEXANDERS KLASSE FUCK AF!
+
 public class martn {
 
 	public static void main(String[] args) {
@@ -8,6 +10,7 @@ public class martn {
 
 		System.out.println("martin er soed som en i helvede");
 		System.out.println("Det var du da traels");
-		System.out.println("tror sgu ikke der er nogen der synes han er træls, med mindre du hedder lillepeter og han siger nej");
+		System.out.println(
+				"tror sgu ikke der er nogen der synes han er trï¿½ls, med mindre du hedder lillepeter og han siger nej");
 	}
 }
