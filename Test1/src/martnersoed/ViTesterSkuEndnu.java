2d2ec1f93cde874591bd415805f8c26ec9e5c229
Alexander Skou <alexander.skou@gmail.com>
2018-09-05 15:14:09 +0200

    Tilføjet

--- Test1/src/martnersoed/ViTesterSkuEndnu.java
+++ Test1/src/martnersoed/ViTesterSkuEndnu.java
@@ -4,6 +4,7 @@ public class ViTesterSkuEndnu {
 	public static void main(String[] args) {
 		System.out.println("Test");
 		System.out.println("Ny test");
+		System.out.println("ny test 2");
 	}
 
 }
