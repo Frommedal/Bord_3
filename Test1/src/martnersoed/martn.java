1d320390466e68246db29fa86f6e0e97a1341517
Alexander Skou <alexander.skou@gmail.com>
2018-09-05 14:57:20 +0200

    Jeg har rettet ÆØÅ.

---
+++ Test1/src/martnersoed/martn.java
@@ -0,0 +1,12 @@
+package martnersoed;
+
+public class martn {
+
+	public static void main(String[] args) {
+
+		// TODO Auto-generated constructor stub
+
+		System.out.println("martin er soed som en i helvede");
+		System.out.println("Det var du da traels");
+	}
+}
